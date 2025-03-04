bf2c00d133cbb569079fecd5f7d5e174c4170feb
asmasher80 <xycheng@fjirsm.ac.cn>
2025-03-04 10:59:37 +0800

    copying files

--- src/atomate2/artatop/flows/artatop.py
+++ src/atomate2/artatop/flows/artatop.py
@@ -10,7 +10,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 from pathlib import Path
 
-from jobflow import Flow, Maker
+from jobflow import Flow, Maker, job
 from pymatgen.core import Structure
 from atomate2.common.files import copy_files
 
@@ -39,7 +39,7 @@ class ArtatopWorkflowMaker(Maker):
         
         # Step 1: Relaxation Job
         relax_flow = self.relax_maker.make(structure=structure)
-        relax_dir = relax_job.output.dir_name
+        relax_dir = relax_flow.output.dir_name
 
         # Step 2: Optics Job (after relaxation)
         optics_flow = self.optics_maker.make(
@@ -47,19 +47,18 @@ class ArtatopWorkflowMaker(Maker):
             prev_dir=relax_flow.output.dir_name,
         )
         
-<<<<<<< HEAD
+
         print('hello')
         print(dir(optics_flow))
         artatop_jobs = get_artatop_jobs(optics_flow.output.dir_name)
-=======
         @job
         def run_artatop_jobs(optics_output):
             """Runs ARTATOP jobs after optics completes."""
+            opics_output_path = str(optics_output)
             return get_artatop_jobs(optics_output)
 
 
         artatop_jobs = get_artatop_jobs(optics_flow.output)
->>>>>>> 93df4c81 (copying files)
 
         # **4. Return the Final Flow**
         return Flow(
